8a81c33632e2e6b792abc5633d7bf79f6a1445e3
nafiz678 <nafizulikram3@gmail.com>
2026-05-12 20:03:12 +0600

    fix: add ESM entrypoint import extensions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { serve } from '@hono/node-server'
-import { env } from './config/env'
-import app from './app'
+import { env } from './config/env.js'
+import app from './app.js'
 
 serve(
   {
